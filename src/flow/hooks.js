99654728f4590785d8ff7415f95eca16683b080d
DoParkEQ <do.park@eqworks.com>
2020-09-12 00:27:05 -0400

    flow/panel - internal logic refactored

--- src/flow/hooks.js
+++ src/flow/hooks.js
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useMemo, useState, useEffect } from 'react'
 
 import { transform, buildLayout } from './utils'
 
@@ -11,4 +11,28 @@ export const useElements = ({ data, config: { layout }, width, height }) => {
   )
 
   return elements
+}
+
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window
+  return {
+    width,
+    height,
+  }
+}
+
+export const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions())
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return windowDimensions
 }
