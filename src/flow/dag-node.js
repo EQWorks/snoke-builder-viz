db56d1e25b570f9df4db99c051c7ad6962e8b6a6
DoParkEQ <do.park@eqworks.com>
2020-09-14 00:34:22 -0400

    flow - node style revised

--- src/flow/dag-node.js
+++ src/flow/dag-node.js
@@ -1,41 +1,49 @@
-import React, { createElement } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-
-import { styled, setup } from 'goober'
 import { Handle, useStoreState } from 'react-flow-renderer'
-
 import { Dot } from './common'
+import { makeStyles } from '@material-ui/core/styles'
+import { Typography } from '@eqworks/react-labs'
+import clsx from 'clsx'
 
+const useStyles = makeStyles((theme) => ({
+  nodeDefault: {
+    zIndex: 1,
+    maxWidth: 275,
+    overflow: 'auto',
+    padding: theme.spacing(2),
+    borderRadius: 10,
+    transition: 'all .4s',
+    border: `solid 1px ${theme.palette.secondary[400]}`,
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    '&:hover': {
+      border: `solid 1px ${theme.palette.primary[200]}`,
+    },
+  },
+  nodeActive: {
+    border: `solid 1px ${theme.palette.primary[700]}`,
+  },
+  text: {
+    display: 'inline-block',
+    marginLeft: theme.spacing(1),
+  },
+}))
 
-setup(createElement)
-
-export const NodeContent = styled('div')`
-  max-width: 275px;
-  overflow: auto;
-  padding: 1rem;
-  font-size: 0.9rem;
-  border-radius: 10px;
-  border: solid 1px ${({ selected }) => selected ? '#0075ff' : '#bdbdbd'};
-  transition: background .2s;
-  &:hover {
-    background-color: #e2f3ff;
-    border: solid 1px #8bceff;
-  }
-`
 
 const DAGNode = ({ id, data: { name, sub, level, period, dag = {} } }) => {
   const [{ id: selected } = {}] = useStoreState((store) => store.selectedElements || [])
-
+  const classes = useStyles()
   return (
     <>
       {level !== 0 && (<Handle type='target' position='left' />)}
-      <NodeContent selected={selected === id}>
-        <Dot state={dag.state} />
-        {' '}
-        {name}
-        {period ? ` (${period})` : ''}
-        {sub ? `: ${sub}` : ''}
-      </NodeContent>
+      <div className={clsx(classes.nodeDefault, { [classes.nodeActive]: selected === id })} selected={selected === id}>
+        <Dot state={dag.state}></Dot>
+        <Typography variant="body1" className={classes.text}>
+          {name}
+          {period ? ` (${period})` : ''}
+          {sub ? `: ${sub}` : ''}
+        </Typography>
+      </div>
       {level !== 3 && (<Handle type='source' position='right' />)}
     </>
   )
